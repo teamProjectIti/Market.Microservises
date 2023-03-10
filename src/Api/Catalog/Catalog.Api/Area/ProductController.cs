80ba6a0b20d89f94bb794684bc239f37aed20c3b
BeshoyAmen <BeshoyAmen@DESKTOP-I9O35O8>
2023-03-10 09:40:32 -0800

    add controller product

---
+++ src/Api/Catalog/Catalog.Api/Area/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Catalog.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using static Catalog.Api.repositery.Product.IMongoRepository;
+using System;
+using Data.Entities.Catalog.Products;
+using Dto.Catalog.Product;
+using AutoMapper;
+
+namespace Catalog.Api.Area
+{
+    public class ProductController : BaseController
+    {
+        private readonly IMongoRepository<Product> _peopleRepository;
+        private readonly IMapper _mapper;
+
+        public ProductController(IMongoRepository<Product> peopleRepository, IMapper mapper)
+        {
+            _peopleRepository = peopleRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost("registerPerson")]
+        public async Task AddPerson(ProductDto model)
+        {
+
+            var result = _mapper.Map<Product>(model);
+
+            await _peopleRepository.InsertOneAsync(result);
+        }
+
+        [HttpGet("getProductData")]
+        public IEnumerable<string> getProductData()
+        {
+            var people = _peopleRepository.FilterBy(
+                filter => filter.Name != "test",
+                projection => projection.Name
+            );
+            return people;
+        }
+    }
+}
