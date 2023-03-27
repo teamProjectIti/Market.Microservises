a6dbc0ad74cdab19e6b051039a0a4f281388af0a
BeshoyAmen <beshoyamen5@gmail.com>
2023-03-27 12:03:24 -0700

    add api discound

---
+++ src/Api/Discound/Descound.Api/Area/DiscoundController.cs
@@ -0,0 +1,15 @@
+﻿using Descound.Api.Controllers;
+
+namespace Descound.Api.Area
+{
+    public class DiscoundController : BaseController
+    {
+
+
+        public DiscoundController()
+        {
+
+        }
+
+    }
+}
---
+++ src/Api/Discound/Descound.Api/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Descound.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+    }
+}
