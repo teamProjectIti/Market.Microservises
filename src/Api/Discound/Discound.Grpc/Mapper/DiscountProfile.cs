594c82f6472d9d4948eedc31490a13b179481569
BeshoyAmen <beshoyamen5@gmail.com>
2023-04-10 13:25:00 -0700

    add service Grpc Http2

---
+++ src/Api/Discound/Discound.Grpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Discound.Grpc.Entities;
+using Discound.Grpc.Protos;
+
+namespace Discound.Grpc.Mapper
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/Api/Discound/Discound.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discound.Grpc.Entities;
+
+namespace Discound.Grpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
