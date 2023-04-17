a39361d9d5afa1e359b5639a47c7702339c50202
BeshoyAmen <beshoyamen5@gmail.com>
2023-04-17 07:40:47 -0700

    create class order with baseEntity

---
+++ src/Api/Ordering/Ordering.Doman/Common/EntityBase/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Doman.Common.EntityBase
+{
+    public abstract class BaseEntity
+    {
+        public long Id { get; protected set; }
+        public string CreatedBy { get; set; }=string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; } = string.Empty;
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
