b131fec36ef5480b976c4512989d4fda39d4a27b
BeshoyAmen <beshoyamen5@gmail.com>
2023-04-18 13:22:28 -0700

    connection basket api wit grpc

--- src/Api/Ordering/Ordering.Application/Contracts/Persistence/Generic/IAsyncRepository.cs
+++ src/Api/Ordering/Ordering.Application/Contracts/Persistence/Generic/IAsyncRepository.cs
@@ -15,6 +15,11 @@ namespace Ordering.Application.Contracts.Persistence.Generic
                                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                                        List<Expression<Func<T, object>>> includes = null,
                                        bool disableTracking = true);
+
+        Task<T> GetEntityAsync(Expression<Func<T, bool>> predicate,
+                                       List<Expression<Func<T, object>>> includes = null,
+                                       bool disableTracking = true);
+
         Task<T> GetByIdAsync(int id);
         Task<T> AddAsync(T entity);
         Task UpdateAsync(T entity);
