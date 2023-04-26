db72d6e6644ad1f69829d007dcba19ada665c0a7
BeshoyAmen <beshoyamen5@gmail.com>
2023-04-26 08:13:15 -0700

    Add Order Context With Soft Delete

---
+++ src/Api/Ordering/Ordering.infrastructure/Persistence/MyContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Doman.Common.Entities;
+using Ordering.Doman.Common.EntityBase;
+using System.Collections.Generic;
+
+namespace Ordering.infrastructure.Persistence
+{
+    public class MyContext:DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Orders> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "swn";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "swn";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
