2d0b7ff4667af7cfa93a1853d9974408b5f634c7
BeshoyAmen <beshoyamen5@gmail.com>
2023-05-06 04:28:54 -0700

    add mapper

--- src/Api/Ordering/Ordering.Api/EventBusConsumer/BasketCheckoutConsumer.cs
+++ src/Api/Ordering/Ordering.Api/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -5,25 +5,25 @@ using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 
 namespace Ordering.Api.EventBusConsumer
 {
-    public class BasketCheckoutConsumer: IConsumer<BasketCheckoutEvent>
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
     {
         private readonly IMediator _mediator;
-    private readonly IMapper _mapper;
-    private readonly ILogger<BasketCheckoutConsumer> _logger;
+        private readonly IMapper _mapper;
+        private readonly ILogger<BasketCheckoutConsumer> _logger;
 
-    public BasketCheckoutConsumer(IMediator mediator, IMapper mapper, ILogger<BasketCheckoutConsumer> logger)
-    {
-        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-    }
+        public BasketCheckoutConsumer(IMediator mediator, IMapper mapper, ILogger<BasketCheckoutConsumer> logger)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
 
-    public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
-    {
-        var command = _mapper.Map<CheckoutOrderCommand>(context.Message);
-        var result = await _mediator.Send(command);
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var command = _mapper.Map<CheckoutOrderCommand>(context.Message);
+            var result = await _mediator.Send(command);
 
-        _logger.LogInformation("BasketCheckoutEvent consumed successfully. Created Order Id : {newOrderId}", result);
-    }
+            _logger.LogInformation("BasketCheckoutEvent consumed successfully. Created Order Id : {newOrderId}", result);
+        }
     }
 }
